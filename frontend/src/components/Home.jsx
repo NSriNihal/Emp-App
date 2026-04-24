3919e1e344a3360b03c9d241885787d1991910d4
Nimishakavi Sri Nihal <nimishakavisrinihal@gmail.com>
2026-04-24 12:59:49 +0530

    Update Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -15,15 +15,16 @@ function Home() {
   const {counter, changeCounter} = useContext(counterContextObj)
   return (
     <div className='gap-5 mx-3'>
-      <h1 className='text-4xl gap-5'>Counter:{counter}</h1>
-      <button onClick={changeCounter} className='bg-amber-200 p-5 gap-5'>change</button>
+      /* {/* <h1 className='text-4xl gap-5'>Counter:{counter}</h1> */} */
+       <h1 className='text-4xl gap-5'>HOME</h1> 
+      {/* <button onClick={changeCounter} className='bg-amber-200 p-5 gap-5'>change</button>
       
       <h1 className='text-4xl gap-5'>Counter:{newCounter}</h1>
       <button onClick={incrementCounter} className='bg-amber-200 p-5 gap-5'>Increase increment counter</button>
       <button onClick={decrementCounter} className='bg-amber-100 p-5'>Decrement increment counter</button>
       <button onClick={resetto500} className='bg-amber-300 p-5'>Reset to 500</button>
       <button onClick={incrementCounterBy20} className='bg-amber-400 p-5'>Increment by 20</button>
-      <Test/>
+      <Test/> */}
     </div>
   )
 }
